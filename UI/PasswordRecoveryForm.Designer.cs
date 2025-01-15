363dfb57c79f07f3a9a05e467f407f7b9f29f354
bax082024 <bax082024@gmail.com>
2025-01-16 00:08:20 +0100

    added layout for passwordrecoveryform

--- UI/PasswordRecoveryForm.Designer.cs
+++ UI/PasswordRecoveryForm.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PasswordRecoveryForm";
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            lblStatus = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(lblStatus);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(50, 55);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(358, 303);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(101, 64);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(141, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(101, 154);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(141, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(122, 18);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(13, 15);
+            lblStatus.TabIndex = 2;
+            lblStatus.Text = "''";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(132, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(101, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Secret Question";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(132, 93);
+            button1.Name = "button1";
+            button1.Size = new Size(62, 25);
+            button1.TabIndex = 5;
+            button1.Text = "next";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(148, 194);
+            button2.Name = "button2";
+            button2.Size = new Size(62, 25);
+            button2.TabIndex = 6;
+            button2.Text = "recover";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(148, 225);
+            button3.Name = "button3";
+            button3.Size = new Size(62, 25);
+            button3.TabIndex = 7;
+            button3.Text = "cancel";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // PasswordRecoveryForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(459, 407);
+            Controls.Add(panel1);
+            Name = "PasswordRecoveryForm";
+            Text = "PasswordRecoveryForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label lblStatus;
+        private TextBox textBox2;
+        private TextBox textBox1;
     }
 }
