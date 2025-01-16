9bc3c6330679e8344c9bc47951fda8b38ee6d7ba
hee082024kh <hee082024@gmail.com>
2025-01-16 14:39:34 +0100

    Fixed possible null reference

--- Models/FriendList.cs
+++ Models/FriendList.cs
@@ -13,20 +13,23 @@ namespace ChatApp_SkoleProsjekt.Models
             {
                 string jsonContent = File.ReadAllText(path);
                 UserList = JsonSerializer.Deserialize<List<User>>(jsonContent);
-                User? user = UserList.FirstOrDefault(u => u.ID == friend.ID);
-                if (user != null)
+                if (UserList != null)
                 {
-                    user.Username = friend.Username;
-                    user.ID = friend.ID;
-                    friendList.Add(friend);
+                    User? user = UserList.FirstOrDefault(u => u.ID == friend.ID);
+                    if (user != null)
+                    {
+                        user.Username = friend.Username;
+                        user.ID = friend.ID;
+                        friendList.Add(friend);
+                    }
+                    string updatedJson = JsonSerializer.Serialize(friendList, new JsonSerializerOptions { WriteIndented = true });
+                    string pathNew = "FriendList.json";
+                    File.WriteAllText(pathNew, updatedJson);
                 }
-                string updatedJson = JsonSerializer.Serialize(friendList, new JsonSerializerOptions { WriteIndented = true });
-                string pathNew = "FriendList.json";
-                File.WriteAllText(pathNew, updatedJson);
-            }
-            else
-            {
-                Console.WriteLine("User does not exist");
+                else
+                {
+                    Console.WriteLine("User does not exist");
+                }
             }
         }
 
