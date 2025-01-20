83aa99d48d078d91655a86186f705c47e6efc40c
wam0824 <wam082024@gmail.com>
2025-01-20 12:19:16 +0100

    Started on ChatForm

--- UI/ChatForm.Designer.cs
+++ UI/ChatForm.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ChatForm";
+            sendButton = new Button();
+            messageListBox = new ListBox();
+            messageTextBox = new TextBox();
+            SendButton1 = new Button();
+            SuspendLayout();
+            // 
+            // sendButton
+            // 
+            sendButton.BackColor = SystemColors.Control;
+            sendButton.Location = new Point(1054, 692);
+            sendButton.Margin = new Padding(4, 5, 4, 5);
+            sendButton.Name = "sendButton";
+            sendButton.Size = new Size(71, 35);
+            sendButton.TabIndex = 0;
+            sendButton.Text = "Send";
+            sendButton.UseVisualStyleBackColor = false;
+            // 
+            // messageListBox
+            // 
+            messageListBox.BackColor = SystemColors.ScrollBar;
+            messageListBox.FormattingEnabled = true;
+            messageListBox.ItemHeight = 25;
+            messageListBox.Location = new Point(231, -1);
+            messageListBox.Margin = new Padding(4, 5, 4, 5);
+            messageListBox.Name = "messageListBox";
+            messageListBox.Size = new Size(546, 479);
+            messageListBox.TabIndex = 1;
+            // 
+            // messageTextBox
+            // 
+            messageTextBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            messageTextBox.Location = new Point(231, 488);
+            messageTextBox.Margin = new Padding(4, 5, 4, 5);
+            messageTextBox.Name = "messageTextBox";
+            messageTextBox.Size = new Size(429, 31);
+            messageTextBox.TabIndex = 2;
+            // 
+            // SendButton1
+            // 
+            SendButton1.Location = new Point(677, 488);
+            SendButton1.Name = "SendButton1";
+            SendButton1.Size = new Size(75, 31);
+            SendButton1.TabIndex = 3;
+            SendButton1.Text = "Send";
+            SendButton1.UseVisualStyleBackColor = true;
+            SendButton1.Click += SendButton1_Click;
+            // 
+            // ChatForm
+            // 
+            AcceptButton = SendButton1;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(778, 544);
+            Controls.Add(SendButton1);
+            Controls.Add(sendButton);
+            Controls.Add(messageTextBox);
+            Controls.Add(messageListBox);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "ChatForm";
+            Text = "ChatForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button sendButton;
+        private ListBox messageListBox;
+        private TextBox messageTextBox;
+        private Button SendButton1;
     }
 }
--- UI/ChatForm.cs
+++ UI/ChatForm.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using ChatApp_SkoleProsjekt.Models;
+using ChatApp_SkoleProsjekt.Services;
+using ChatApp_SkoleProsjekt.UI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,40 @@ namespace ChatApp_SkoleProsjekt.UI
 {
     public partial class ChatForm : Form
     {
+        private TcpClient? _client; // Nullable tTcpClient
+        private TcpListener? _listener; // Nullable TcpListener
+        private NetworkStream? _stream; // Nullable NetworkStream   
+        private readonly User _currentUser;
+
+        private readonly AuthenticationHelper _authenticationHelper;
+        
         public ChatForm()
         {
             InitializeComponent();
+
+            _currentUser = currentUser;
+            _authenticationHelper = new AuthenticationHelper();
+            this.Load += ChatForm_Load;
+            //this.FormClosing += ChatForm_FormClosing;
+            //DisplayUserDetails();
+        }
+
+        private void ChatForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SendButton1_Click(object sender, EventArgs e)
+        {
+            string message = messageTextBox.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                messageListBox.Items.Add($"You: {message}");
+
+                messageTextBox.Clear();
+            }
+
         }
     }
 }
