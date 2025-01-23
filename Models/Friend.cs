50d19bb0498ccd298a663b03206dfd449d95ccff
hee082024kh <hee082024kh@gmail.com>
2025-01-23 10:35:01 +0100

    Removed test commit

--- Models/Friend.cs
+++ Models/Friend.cs
@@ -2,7 +2,6 @@ namespace ChatApp_SkoleProsjekt.Models
 {
   public class Friend
   {
-    // Test
     public string Username { get; set; } = string.Empty;
     public Guid ID { get; set; } = Guid.NewGuid();
   }
