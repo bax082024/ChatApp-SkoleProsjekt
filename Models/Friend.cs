b9f7ada0ae34727e0924519f5ead55791b65cfa3
hee082024kh <hee082024kh@gmail.com>
2025-01-23 10:34:23 +0100

    Test commit

--- Models/Friend.cs
+++ Models/Friend.cs
@@ -2,6 +2,7 @@ namespace ChatApp_SkoleProsjekt.Models
 {
   public class Friend
   {
+    // Test
     public string Username { get; set; } = string.Empty;
     public Guid ID { get; set; } = Guid.NewGuid();
   }
