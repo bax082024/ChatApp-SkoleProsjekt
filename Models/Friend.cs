d20d2be43b6b9c4ed3ccaebda0d97a790d4bebe4
wam0824 <wam082024@gmail.com>
2025-02-14 09:51:05 +0100

    Endret friend.cs

--- Models/Friend.cs
+++ Models/Friend.cs
@@ -1,13 +1,19 @@
-using System.Xml.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChatApp_SkoleProsjekt.Models
 {
   public class Friend
   {
     public string Username { get; set; } = string.Empty;
-    public Guid ID { get; set; } = Guid.NewGuid();
+    public Guid ID { get; set; }
+    public string IPAddress { get; set; } = string.Empty;
+    public int Port { get; set; }
 
-    public override string ToString()
+        public override string ToString()
         {
             return Username; // Display the friend's name in the FriendListBox
         }
