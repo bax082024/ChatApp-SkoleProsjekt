6a6c0c0f11bdfaedce10984a5ca6385769efe459
bax082024 <bax082024@gmail.com>
2025-02-14 11:33:15 +0100

    fixed btn links

--- UI/ResetPasswordForm.Designer.cs
+++ UI/ResetPasswordForm.Designer.cs
@@ -30,11 +30,11 @@
         {
             lblStatus = new Label();
             panel1 = new Panel();
-            panel2 = new Panel();
-            txtNewPassword = new TextBox();
-            txtConfirmPassword = new TextBox();
-            btnReset = new Button();
             btnCancel = new Button();
+            btnReset = new Button();
+            txtConfirmPassword = new TextBox();
+            txtNewPassword = new TextBox();
+            panel2 = new Panel();
             statusStrip1 = new StatusStrip();
             toolStripStatusLabel1 = new ToolStripStatusLabel();
             panel1.SuspendLayout();
@@ -46,10 +46,9 @@
             // 
             lblStatus.AutoSize = true;
             lblStatus.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblStatus.Location = new Point(88, 13);
-            lblStatus.Margin = new Padding(4, 0, 4, 0);
+            lblStatus.Location = new Point(62, 8);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(217, 38);
+            lblStatus.Size = new Size(149, 25);
             lblStatus.TabIndex = 1;
             lblStatus.Text = "Reset Password";
             // 
@@ -61,76 +60,73 @@
             panel1.Controls.Add(txtConfirmPassword);
             panel1.Controls.Add(txtNewPassword);
             panel1.Controls.Add(lblStatus);
-            panel1.Location = new Point(10, 8);
-            panel1.Margin = new Padding(4, 5, 4, 5);
+            panel1.Location = new Point(7, 5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(394, 366);
+            panel1.Size = new Size(276, 220);
             panel1.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.DimGray;
-            panel2.BorderStyle = BorderStyle.Fixed3D;
-            panel2.Controls.Add(panel1);
-            panel2.Location = new Point(80, 71);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(420, 388);
-            panel2.TabIndex = 3;
-            // 
-            // txtNewPassword
-            // 
-            txtNewPassword.BackColor = SystemColors.Info;
-            txtNewPassword.Location = new Point(103, 87);
-            txtNewPassword.Margin = new Padding(4, 5, 4, 5);
-            txtNewPassword.Name = "txtNewPassword";
-            txtNewPassword.PlaceholderText = "Username";
-            txtNewPassword.Size = new Size(180, 31);
-            txtNewPassword.TabIndex = 11;
-            // 
-            // txtConfirmPassword
-            // 
-            txtConfirmPassword.BackColor = SystemColors.Info;
-            txtConfirmPassword.Location = new Point(103, 151);
-            txtConfirmPassword.Margin = new Padding(4, 5, 4, 5);
-            txtConfirmPassword.Name = "txtConfirmPassword";
-            txtConfirmPassword.PlaceholderText = "Confirm Password";
-            txtConfirmPassword.Size = new Size(180, 31);
-            txtConfirmPassword.TabIndex = 12;
-            // 
-            // btnReset
-            // 
-            btnReset.BackColor = Color.LightBlue;
-            btnReset.FlatStyle = FlatStyle.Flat;
-            btnReset.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnReset.Location = new Point(130, 209);
-            btnReset.Margin = new Padding(4, 5, 4, 5);
-            btnReset.Name = "btnReset";
-            btnReset.Size = new Size(121, 45);
-            btnReset.TabIndex = 13;
-            btnReset.Text = "Reset";
-            btnReset.UseVisualStyleBackColor = false;
             // 
             // btnCancel
             // 
             btnCancel.BackColor = Color.LightBlue;
             btnCancel.FlatStyle = FlatStyle.Flat;
             btnCancel.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnCancel.Location = new Point(130, 275);
-            btnCancel.Margin = new Padding(4, 5, 4, 5);
+            btnCancel.Location = new Point(91, 165);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(121, 45);
+            btnCancel.Size = new Size(85, 27);
             btnCancel.TabIndex = 14;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.LightBlue;
+            btnReset.FlatStyle = FlatStyle.Flat;
+            btnReset.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnReset.Location = new Point(91, 125);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(85, 27);
+            btnReset.TabIndex = 13;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.BackColor = SystemColors.Info;
+            txtConfirmPassword.Location = new Point(72, 91);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.PlaceholderText = "Confirm Password";
+            txtConfirmPassword.Size = new Size(127, 23);
+            txtConfirmPassword.TabIndex = 12;
+            // 
+            // txtNewPassword
+            // 
+            txtNewPassword.BackColor = SystemColors.Info;
+            txtNewPassword.Location = new Point(72, 52);
+            txtNewPassword.Name = "txtNewPassword";
+            txtNewPassword.PlaceholderText = "Username";
+            txtNewPassword.Size = new Size(127, 23);
+            txtNewPassword.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DimGray;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(panel1);
+            panel2.Location = new Point(56, 43);
+            panel2.Margin = new Padding(2, 2, 2, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(295, 234);
+            panel2.TabIndex = 3;
             // 
             // statusStrip1
             // 
             statusStrip1.ImageScalingSize = new Size(24, 24);
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
-            statusStrip1.Location = new Point(0, 539);
+            statusStrip1.Location = new Point(0, 321);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Padding = new Padding(1, 0, 20, 0);
-            statusStrip1.Size = new Size(585, 32);
+            statusStrip1.Size = new Size(410, 22);
             statusStrip1.TabIndex = 7;
             statusStrip1.Text = "Chat App";
             // 
@@ -138,18 +134,17 @@
             // 
             toolStripStatusLabel1.BackColor = Color.Transparent;
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(87, 25);
+            toolStripStatusLabel1.Size = new Size(57, 17);
             toolStripStatusLabel1.Text = "Chat App";
             // 
             // ResetPasswordForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(585, 571);
+            ClientSize = new Size(410, 343);
             Controls.Add(statusStrip1);
             Controls.Add(panel2);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "ResetPasswordForm";
             Text = "ResetPasswordForm";
             panel1.ResumeLayout(false);
